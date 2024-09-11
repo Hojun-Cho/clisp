ba39b84914e9bb48adf4830a8f860a12fa0915d5
yoyo <ghwns674@gmail.com>
2024-09-11 17:21:38 +0900

    add and, or macro

--- lib/lib.lisp
+++ lib/lib.lisp
@@ -1,8 +1,24 @@
 (macro defun (name args body) 
 		`(define ,name (lambda ,args ,body)))
 
+(macro cond (expr . rest)
+	(if (not expr)
+		nil
+		(let ((test  (car expr)))
+			`(if ,test
+				(progn ,test ,@(cdr expr))
+				(cond ,@rest)))))
+
+(macro and (expr . rest)
+	(if (not rest)
+		expr
+		(if (cond (not expr) nil)
+			`(and ,@rest))))
+
+(macro or (expr . rest)
+	(if rest
+		(cond (expr) (`(or ,@rest)))
+		expr))
+
 (defun list (x . y) (cons x y))
 
-(macro cond (expr . rest)
-	(if (not expr) nil
-		`(if ,(car expr) (progn ,@expr) (cond ,@rest))))
